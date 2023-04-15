22ad525e2ccd78dd32f100bf2361c8bd2a29ea3d
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2023-04-15 18:41:15 +0530

    Time: 325 ms (46.07%), Space: 18.5 MB (43.19%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -3,22 +3,37 @@ class Solution {
     int col;
     int n;
     vector<vector<int>>coin;
-    int maxi=INT_MIN;
-    void dp(){
-        for(int i = 1; i <= n; i++){
-            int pileSize = pile[i-1].size();
-            for(int j = 1;  j <= col; j++){
-                int sum = 0;
+    // int maxi=INT_MIN;
+    int dp(int idx, int k){
+//         for(int i = 1; i <= n; i++){
+//             int pileSize = pile[i-1].size();
+//             for(int j = 1;  j <= col; j++){
+//                 int sum = 0;
                 
-                for(int k=0;k<pileSize;k++)
-                {
-                    if((k+1)>j)break;
-                    sum+=pile[i-1][k];
-                    coin[i][j] = max({coin[i][j],sum+coin[i-1][j-(k+1)],coin[i-1][j]});
-                    maxi = max(maxi,coin[i][j]);
-                }
-                }
+//                 for(int k=0;k<pileSize;k++)
+//                 {
+//                     if((k+1)>j)break;
+//                     sum+=pile[i-1][k];
+//                     coin[i][j] = max({coin[i][j],sum+coin[i-1][j-(k+1)],coin[i-1][j]});
+//                     maxi = max(maxi,coin[i][j]);
+//                 }
+//                 }
+//             }
+        
+        if(idx==n) return 0;
+        
+        if(coin[idx][k]) return coin[idx][k];
+        
+        int not_taken = dp(idx+1, k);
+        
+        int taken = 0, sum = 0;
+        for(int j = 0; j < min((int)pile[idx].size(), k); j++){
+            sum += pile[idx][j];
+            taken = max(taken, sum + dp(idx+1, k - (j+1)));
             }
+        
+        return coin[idx][k] = max(taken, not_taken);
+        
         }
 public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
@@ -26,7 +41,7 @@ public:
         pile = piles; 
         col = k;
         coin = vector<vector<int>>(n+1, vector<int>(k+1, 0));
-        dp();
-        return coin[n][k];
+        
+        return dp(0, k);
     }
 };
