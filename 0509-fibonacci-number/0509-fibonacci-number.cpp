f69bd8ce92dc4fdc40f1c83f02bd23b7689c6850
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2023-04-20 09:37:09 +0530

    Time: 3 ms (70.75%), Space: 6.1 MB (20.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,24 @@
 class Solution {
     vector<int>v;
-    int dp(int n){
-        if(n == 0) return 0;
-        if(n == 1) return 1;
+//     int dp(int n){
+//         if(n == 0) return 0;
+//         if(n == 1) return 1;
         
-        if(v[n]!=-1)  return v[n];
-        return v[n] = dp(n-1) + dp(n-2);
+//         if(v[n]!=-1)  return v[n];
+//         return v[n] = dp(n-1) + dp(n-2);
         
-    }
+//     }
     
 public:
     int fib(int n) {
-        v = vector<int>(n+1, -1);
-        return dp(n);
+        v = vector<int>(n+2, -1);
+        v[0] = 0;
+        v[1] = 1;
+        for(int i = 2; i <=n; i++){
+            v[i] = v[i-1] + v[i-2];
+        }
+        
+        // return dp(n);
+        return v[n];
     }
 };
