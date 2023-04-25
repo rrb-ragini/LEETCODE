6dad75a6cedda5fe4d192c4d2e65fe097d2fa278
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2023-04-25 13:53:17 +0530

    Time: 56 ms (99.56%), Space: 35.3 MB (92.24%) - LeetHub

--- 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.cpp
@@ -1,20 +1,38 @@
 class SmallestInfiniteSet {
     multiset<int>pq;
+    int curr = 1;
+
 public:
     SmallestInfiniteSet() {
-        for(int i = 1; i <= 1000; i++){
-            pq.insert(i);
-        }
+        // for(int i = 1; i <= 1000; i++){
+        //     pq.insert(i);
+        // }
     }
     
     int popSmallest() {
-        int a = *pq.begin();
-        pq.erase(*pq.begin());
-        return a;
+        int x;
+        if(pq.size()){
+            x = *pq.begin(); 
+            pq.erase(x);
+        }
+        else{
+            x = curr;
+            curr++;
+        }
+        return x;
+    
+        // int a = *pq.begin();
+        // pq.erase(*pq.begin());
+        // return a;
     }
     
     void addBack(int num) {
-        pq.insert(num);
+        if(num < curr){
+            pq.insert(num);
+            // curr = num;
+            }
+        // else if(num<*pq.begin())
+        //     pq.insert(num);
     }
 };
 
