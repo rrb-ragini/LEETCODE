c2ef7289dd70db7c6ddb2a4846b5fb30c225d511
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2023-04-28 13:29:41 +0530

    Time: 775 ms (8.25%), Space: 11 MB (46.03%) - LeetHub

---
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    int n, m;
+    vector<bool>visited;
+    vector<string> strs;
+    
+    bool isSimilar(int i, int j){
+        int count = 0;
+        for(int k = 0; k < m; k++)
+            if(strs[i][k]!=strs[j][k])
+                count++;
+        return (count == 0 or count == 2);
+    }
+    
+    void dfs(int i){
+        if(visited[i]) return;
+        visited[i] = true;
+        
+        for(int j = 0; j < n; j++)
+            if(isSimilar(i, j))
+                dfs(j);        
+    }
+public:
+    int numSimilarGroups(vector<string>& str) {
+        strs = str;
+        n = strs.size();
+        m = strs[0].size();
+        visited = vector<bool>(n, false);
+        int res = 0;
+        
+        //dfs
+        for(int i = 0; i < n; i++)
+            if(!visited[i]){
+                dfs(i);
+                res++;
+            }
+        
+        return res;
+    }
+};
