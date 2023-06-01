b7d6aa440db9d4c3981614f250e386887d47e49b
RAGINI BHAYANA <55350452+rrb-ragini@users.noreply.github.com>
2023-06-01 21:00:55 +0530

    Time: 31 ms (16.13%), Space: 46.5 MB (88.91%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(), prices.end());
+        int sum = (prices[0]+prices[1]);
+        if(money-sum>=0) money-=sum;
+        return money;
+    }
+};
